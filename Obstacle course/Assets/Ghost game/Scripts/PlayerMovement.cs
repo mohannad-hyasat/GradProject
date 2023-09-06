c465f2b5ff46214803a81b0ea778eab20b597a2d
hamza <ham20190156@std.psut.edu.jo>
2023-09-06 20:55:59 +0300

    -still crouch brokeeeen

--- Obstacle course/Assets/Ghost game/Scripts/PlayerMovement.cs
+++ Obstacle course/Assets/Ghost game/Scripts/PlayerMovement.cs
@@ -10,30 +10,25 @@ public class PlayerMovement : MonoBehaviour
     public float walkspeed;
     public float runSpeed;
     public float crouchSpeed;
-
+    [HideInInspector]
     public bool isGrounded;
     public LayerMask groundMask;
+   [HideInInspector] 
     public float gravity;
-
-
     private Vector3 moveDir;
     private Vector3 velocity;
-
     [Header("Crouch parameters")]
     public float crouchHeight = 0.5f;
     public float standHeight = 2f;
-    public float timeToCrouch = 0.25f;
+    private const float timeToCrouch = 0.25f;
     public Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
     public Vector3 standingCenter = new Vector3(0, 1f, 0);
     private bool isCrouching = false;
     private bool duringCrouchAnimation;
     private bool shouldCrouch => Input.GetKeyDown(KeyCode.LeftControl) && !duringCrouchAnimation && isGrounded;
-
-    //crouching center point
-
+    [HideInInspector]
     public Animator animator;
-
-    [SerializeField] private CharacterController characterController;
+    private CharacterController characterController;
 
     // Start is called before the first frame update
     void Start()
@@ -43,38 +38,19 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        CrouchHandler();
-        Move();
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.1f, groundMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 0.1f, Color.red);
-            isGrounded = true;
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 0.1f, Color.red);
-            isGrounded = false;
-        }
-
-
-
-    }
-
+    
+    /// <summary>
+    /// function that handles all the movement of the player controller
+    /// </summary>
     private void Move()
     {
-
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = 0f;
         }
-
         float moveZ = Input.GetAxis("Vertical");
         float moveX = Input.GetAxis("Horizontal");
         moveDir = transform.right * moveX + transform.forward * moveZ;
-
         if (isGrounded)
         {
             if (moveDir != Vector3.zero && isCrouching)
@@ -89,7 +65,7 @@ public class PlayerMovement : MonoBehaviour
                 else
                     animator.SetFloat("CRL", moveX);
             }
-            else if (moveDir != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            else if (moveDir != Vector3.zero && !Input.GetKey(KeyCode.LeftShift) && !isCrouching)
             {
                 moveSpeed = walkspeed * Time.deltaTime;
                 animator.SetBool("walk", true);
@@ -101,7 +77,7 @@ public class PlayerMovement : MonoBehaviour
                 animator.SetBool("run", false);
                 animator.SetBool("Crouch", false);
             }
-            else if (moveDir != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            else if (moveDir != Vector3.zero && Input.GetKey(KeyCode.LeftShift) && !isCrouching)
             {
                 moveSpeed = runSpeed * Time.deltaTime;
                 animator.SetBool("run", true);
@@ -119,17 +95,17 @@ public class PlayerMovement : MonoBehaviour
                 animator.SetBool("run", false);
                 animator.SetBool("walk", false);
                 animator.SetBool("Crouch", true);
-                animator.SetFloat("CFB", moveZ);
-                animator.SetFloat("CRL", moveX);
+                animator.SetFloat("CFB", 0f);
+                animator.SetFloat("CRL", 0f);
 
             }
             else if (moveDir == Vector3.zero && !isCrouching)
             {
                 animator.SetBool("run", false);
-                animator.SetBool("walk", true);
+                animator.SetBool("walk", false);
                 animator.SetBool("Crouch", false);
-                animator.SetFloat("FB", moveZ);
-                animator.SetFloat("RL", moveX);
+                animator.SetFloat("FB", 0f);
+                animator.SetFloat("RL", 0f);
 
             }
             characterController.Move(moveDir * moveSpeed);
@@ -137,6 +113,9 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
     }
+    /// <summary>
+    /// handels the crouch function
+    /// </summary>
     private void CrouchHandler()
     {
         if (shouldCrouch)
@@ -144,6 +123,9 @@ public class PlayerMovement : MonoBehaviour
             CrouchStand();
         }
     }
+    /// <summary>
+    /// Switches between crouching and standing
+    /// </summary>
     private async void CrouchStand()
     {
         duringCrouchAnimation = true;
@@ -153,7 +135,6 @@ public class PlayerMovement : MonoBehaviour
         Vector3 currentCenter = characterController.center;
         float stance = isCrouching ? 0f : 1f;
         float currentHeight = characterController.height;
-
         while (timelapsed < timeToCrouch)
         {
             characterController.height = Mathf.Lerp(currentHeight, targetHeight, timelapsed / timeToCrouch);
@@ -163,11 +144,23 @@ public class PlayerMovement : MonoBehaviour
         }
         characterController.height = targetHeight;
         characterController.center = targetCenter;
-
         isCrouching = !isCrouching;
-
-
         duringCrouchAnimation = false;
     }
-
+    void Update()
+    {
+        CrouchHandler();
+        Move();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.1f, groundMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 0.1f, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 0.1f, Color.red);
+            isGrounded = false;
+        }
+    }
 }
