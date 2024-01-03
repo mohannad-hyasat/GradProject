2e2a040f278e7bc69dee4d5514c584ef766e4b8d
hamza <ham20190156@std.psut.edu.jo>
2024-01-04 02:00:37 +0300

    - Added new ghosts
    -added four ghost types
    kikimora
    jinn
    mylinger
    ogbanje
    
    -implemented two ghost types
    kikimora
    ogbanje

--- Obstacle course/Assets/RoomsManager.cs
+++ Obstacle course/Assets/RoomsManager.cs
@@ -16,11 +16,21 @@ public class RoomsManager : MonoBehaviour
     public const float Min_Temp = -15;
     public int DistanceToTrigger;
 
+    [Header("GHost Choice Settings")]
+    public Ghost_Type GhostType;
+    public GameObject Moths;
     private void Awake()
     {
-        Favorite_Room = Rooms[Random.Range(0, Rooms.Length)];
+        Favorite_Room = Rooms[Random.Range(0, Rooms.Length)]; // randomize favorite room
+        GhostType = (Ghost_Type)Random.Range(0, 4);    // randomize ghost type
     }
-  
+    private void Start()
+    {
+        if(GhostType == Ghost_Type.Kikimora || GhostType == Ghost_Type.Ogbanje || GhostType == Ghost_Type.mylingar)
+        {
+            Moths = GameObject.Instantiate(Resources.Load<GameObject>("Moths area"), Favorite_Room);
+        }
+    }
     private void OnDrawGizmos()
     {
         if (Favorite_Room != null)
@@ -30,19 +40,57 @@ public class RoomsManager : MonoBehaviour
         }
     }
 
-    private void Update()
+    public void Kikimora()
     {
         if (DistanceBetweenPlayerAndRoom <= DistanceToTrigger)
         {
+
+            if (Temp_C >= Min_Temp)
+            {
+                Temp_C -= 0.1f;
+            }
+        }
+    }
+    public void Ogbanje()
+    {
+        if (DistanceBetweenPlayerAndRoom <= DistanceToTrigger)
+        {
+
             if (Temp_C <= Max_Temp)
             {
                 Temp_C += 0.1f;
             }
         }
+    }
+    public void GhostIdentifier()
+    {
+        if(GhostType == Ghost_Type.Kikimora)
+        {
+            Kikimora();
+        }
+
+        if (GhostType == Ghost_Type.Ogbanje)
+        {
+            Ogbanje();
+        }
+
+
+
+
+    }
+
+
+    private void Update()
+    {
+       GhostIdentifier();
+
 
     }
 }
 public enum Ghost_Type
 {
-
+    Kikimora,  //freezing / moths
+    Jinn,      //Burning / emf
+    mylingar,  // moths / emf
+    Ogbanje,   //burning/ moths
 }
