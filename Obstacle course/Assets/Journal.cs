5da280e94a29183f9bfb90dede08d450edaa0ded
mohannad-hyasat <hyasat.mohannad@gmail.com>
2024-04-26 15:30:41 +0300

    Journal UI
    finished making a journal UI

---
+++ Obstacle course/Assets/Journal.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Journal : MonoBehaviour
+{
+    public GameObject journal;
+    public static bool isPaused;
+    public static Ghost_Type Ghost_Guess;
+    private void Start()
+    {
+        journal.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J)) 
+        {
+            journal.SetActive(!isPaused);
+            Time.timeScale = isPaused ? 1.0f : 0.0f;
+            Cursor.visible = !isPaused;
+            Cursor.lockState = isPaused ? CursorLockMode.Locked : CursorLockMode.None;
+            isPaused = !isPaused;
+        }
+    }
+
+    public void SetGhostGuess(string GhostType)
+    {
+        if (GhostType == "Kikimora")
+        {
+            Ghost_Guess = Ghost_Type.Kikimora;
+            
+        }
+
+        if (GhostType == "Ogbanje")
+        {
+            Ghost_Guess = Ghost_Type.Ogbanje;
+            
+        }
+
+        if (GhostType == "Jinn")
+        {
+            Ghost_Guess = Ghost_Type.Jinn;
+            
+        }
+        if (GhostType == "mylingar")
+        {
+            Ghost_Guess = Ghost_Type.mylingar;
+            
+        }
+    }
+}
